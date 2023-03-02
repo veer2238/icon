f0b120eab824dc70c749e6adb6b8f825094f0058
veer2238 <114563813+veer2238@users.noreply.github.com>
2023-03-02 10:18:11 +0530

    icon

---
+++ Icon/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BsSearch } from "react-icons/bs";
+
+const App = () => {
+  return (
+    <div >
+    <h2 style={{textAlign:'center'}}>
+    Hi This Icon Is <BsSearch/>
+    </h2>
+    
+    </div>
+  )
+}
+
+export default App
